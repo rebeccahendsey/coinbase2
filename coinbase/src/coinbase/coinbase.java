0397be19742f71e61ea9b89375b6fa13df2a7b33
rebeccahendsey <61711413+rebeccahendsey@users.noreply.github.com>
2022-05-10 09:30:10 -0400

    Hidden passwords and error check on login / sign up page.

--- coinbase/src/coinbase/coinbase.java
+++ coinbase/src/coinbase/coinbase.java
@@ -95,11 +95,13 @@ public class coinbase extends JFrame
             EmailTA.setHorizontalAlignment(JTextField.CENTER);
         
             // Password text field
-            JTextField passTA = new JTextField("");
+            JPasswordField passTA = new JPasswordField(""); 
+            //JTextField passTA = new JTextField("");
             passTA.setBounds(144, 288, 200, 30);
             contentPane.add(passTA);
             passTA.setBorder(border);
             passTA.setForeground(new Color(188, 188, 188));
+            passTA.setEchoChar('*');
             passTA.setHorizontalAlignment(JTextField.CENTER);
 
             // Email label
@@ -146,6 +148,15 @@ public class coinbase extends JFrame
                     
                     return;
                 }
+                char[] passwordFieldChar = passTA.getPassword();
+
+                if (passwordFieldChar == null || passwordFieldChar.length == 0)
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please enter your password.");
+                    
+                    return;
+                }
                 String dataStr = null;
                 dataStr="User : " + emailFieldStr;
      
@@ -181,7 +192,7 @@ public class coinbase extends JFrame
             LnameL.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
             // First name test field
-            JTextField FnameTA = new JTextField("First name");
+            JTextField FnameTA = new JTextField("");
             FnameTA.setBounds(650, 250, 90, 30);
             contentPane.add(FnameTA);
             FnameTA.setBorder(border);
@@ -189,7 +200,7 @@ public class coinbase extends JFrame
             FnameTA.setHorizontalAlignment(JTextField.CENTER);
 
             // Last name text field
-            JTextField LnameTA = new JTextField("Last name ");
+            JTextField LnameTA = new JTextField("");
             LnameTA.setBounds(760, 250, 90, 30);
             contentPane.add(LnameTA);
             LnameTA.setBorder(border);
@@ -197,7 +208,7 @@ public class coinbase extends JFrame
             LnameTA.setHorizontalAlignment(JTextField.CENTER);
             
             // Email text field for create account
-            JTextField EmailTA2 = new JTextField("Email");
+            JTextField EmailTA2 = new JTextField("");
             EmailTA2.setBounds(650, 310, 200, 30);
             contentPane.add(EmailTA2);
             EmailTA2.setBorder(border);
@@ -212,10 +223,12 @@ public class coinbase extends JFrame
             EmailL.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
             // Password text field for create account
-            JTextField passTA2 = new JTextField("Choose password");
+            JPasswordField passTA2 = new JPasswordField("");
+           // JTextField passTA2 = new JTextField("Choose password");
             passTA2.setBounds(650, 370, 200, 30);
             contentPane.add(passTA2);
             passTA2.setBorder(border);
+            passTA2.setEchoChar('*');
             passTA2.setForeground(new Color(188, 188, 188));
             passTA2.setHorizontalAlignment(JTextField.CENTER);
 
@@ -225,44 +238,8 @@ public class coinbase extends JFrame
             contentPane.add(PassL);
             PassL.setBorder(null);
             PassL.setFont(new Font("Tahoma", Font.PLAIN, 10));
-
-            // Create account button
-            JButton btnNewButton_2 = new JButton("Create account");
-            btnNewButton_2.setBackground(Color.BLUE);
-            btnNewButton_2.setBounds(650, 550, 200, 30);
-            contentPane.add(btnNewButton_2);
-            btnNewButton_2.addActionListener(new ActionListener(){
             
-            public void actionPerformed(ActionEvent arg0)
-            {
-                String emailFieldStr2 = EmailTA2.getText();
-                emailFieldStr2        = emailFieldStr2.trim();
-                if (emailFieldStr2 == "" || emailFieldStr2 == null || emailFieldStr2.length() == 0)
-                {
-                    JOptionPane.showMessageDialog(null,
-                                    "Please enter a valid email address.");
-                    
-                    return;
-                }
-                String dataStr = null;
-                dataStr="User : " + emailFieldStr2;
-     
-                fileIO fio = new fileIO("users.txt");
-                fio.wrTransactionData(dataStr);
-                
-                contentPane.setVisible(false); 
-                
-                homeScreen bs; // Reference to the class BuyAndSell
-                bs = new homeScreen(); // Creates the object of class BuyAndSell
-                bs.setVisible(true);
-                
-            }
-            });
-            contentPane.add(btnNewButton_2);
-      
-
-
-            // State text field
+         // State text field
 
             String state[] = {"Select State", "AK", "AZ", "AR", "CA", "CZ", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN",
                                 "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI",
@@ -280,15 +257,96 @@ public class coinbase extends JFrame
             stateL.setFont(new Font("Tahoma", Font.PLAIN, 10));
 
             // Social security number text field
-            JTextField ssn = new JTextField("Social security number");
+            JPasswordField ssn = new JPasswordField("");
+            //JTextField ssn = new JTextField("Social security number");
             ssn.setBounds(650, 490, 200, 30);
             contentPane.add(ssn);
             ssn.setBorder(border);
+            ssn.setEchoChar('*');
             ssn.setForeground(new Color(188, 188, 188));
             ssn.setHorizontalAlignment(JTextField.CENTER);
 
+            // Create account button
+            JButton btnNewButton_2 = new JButton("Create account");
+            btnNewButton_2.setBackground(Color.BLUE);
+            btnNewButton_2.setBounds(650, 550, 200, 30);
+            contentPane.add(btnNewButton_2);
+            btnNewButton_2.addActionListener(new ActionListener(){
+            
+            public void actionPerformed(ActionEvent arg0)
+            {
+            	String FnameStr = FnameTA.getText();
+            	if (FnameStr == "" || FnameStr == null || FnameStr.length() == 0)
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please enter your first name.");
+                    
+                    return;
+                }
+            	String LnameStr = LnameTA.getText();
+            	if (LnameStr == "" || LnameStr == null || LnameStr.length() == 0)
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please enter your last name.");
+                    
+                    return;
+                }
+                String emailFieldStr2 = EmailTA2.getText();
+                emailFieldStr2        = emailFieldStr2.trim();
+                if (emailFieldStr2 == "" || emailFieldStr2 == null || emailFieldStr2.length() == 0)
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please enter a valid email address.");
+                    
+                    return;
+                }
+                char[] passwordFieldChar = passTA2.getPassword();
+
+                if (passwordFieldChar == null || passwordFieldChar.length == 0)
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please enter your password.");
+                    
+                    return;
+                }
+                String stateStr = (String) stateF.getSelectedItem();
+
+                if (stateStr == null || stateStr == "Select State")
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please select a state.");
+                    
+                    return;
+                }
+                char[] ssnFieldChar = ssn.getPassword();
+
+                if (ssnFieldChar == null || ssnFieldChar.length == 0)
+                {
+                    JOptionPane.showMessageDialog(null,
+                                    "Please enter your ssn.");
+                    
+                    return;
+                }
+               
+                String dataStr = null;
+                dataStr="User : " + emailFieldStr2;
+     
+                fileIO fio = new fileIO("users.txt");
+                fio.wrTransactionData(dataStr);
+                
+                contentPane.setVisible(false); 
+                
+                homeScreen bs; // Reference to the class BuyAndSell
+                bs = new homeScreen(); // Creates the object of class BuyAndSell
+                bs.setVisible(true);
+                
+            }
+            });
+            contentPane.add(btnNewButton_2);
+      
+
             // Social security label
-            JLabel ssnL = new JLabel("Social security number");
+            JLabel ssnL = new JLabel("Social Security Number");
             ssnL.setBounds(650, 465, 200, 30);
             contentPane.add(ssnL);
             ssnL.setBorder(null);
