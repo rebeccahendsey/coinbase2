56d07ec0be23948a08dd4bc7adb2152c313d705f
rebeccahendsey <61711413+rebeccahendsey@users.noreply.github.com>
2022-05-10 15:37:32 -0400

    Fixed the states in the sign up section.

--- coinbase/src/coinbase/coinbase.java
+++ coinbase/src/coinbase/coinbase.java
@@ -241,8 +241,8 @@ public class coinbase extends JFrame
             
          // State text field
 
-            String state[] = {"Select State", "AK", "AZ", "AR", "CA", "CZ", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN",
-                                "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI",
+            String state[] = {"Select State", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN",
+                                "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",
                                 "VA", "WA", "WV", "WI", "WY"};
             @SuppressWarnings({ "unchecked", "rawtypes" })
             JComboBox stateF = new JComboBox(state);
